2ba55943c42f3dd1e3ce0b58a248b6a22c09aa44
Johannes Stelzer <jsdmail@gmail.com>
2025-07-17 16:25:18 +0100

    Update SoundNetworkStreamer initialization to use 'stable' profile for improved audio streaming performance.

--- spatial_audio_ai/tools/client.py
+++ spatial_audio_ai/tools/client.py
@@ -352,7 +352,7 @@ class BlackHoleStereoRelayer:
         self.audio_deque = deque(maxlen=self.max_queue_size)
 
         # Initialize the SoundNetworkStreamer (using real connection)
-        self.sound_streamer = SoundNetworkStreamer()
+        self.sound_streamer = SoundNetworkStreamer(profile='stable')
 
         # Thread control
         self._recording_thread = None
